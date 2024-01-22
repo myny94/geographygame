8d2033af704edefe290fec4e977370cc0bc32831
Nayeong Song <na.song@tuni.fi>
2024-01-22 15:54:10 +0900

    Synchronize the serverEvent schema as backend

--- src/types/ServerEvent.ts
+++ src/types/ServerEvent.ts
@@ -6,15 +6,20 @@ export const ServerEventSchema = z.discriminatedUnion('type', [
     type: z.literal('end_game'),
   }),
   z.object({
+    type: z.literal('wrong_answer'),
+    remainingLives: z.number(),
+  }),
+  z.object({
     type: z.literal('question'),
     questionId: z.string(),
     geoImageUrl: z.string().url(),
+    remainingLives: z.number(),
   }),
   z.object({
     type: z.literal('give_hint'),
     questionId: z.string(),
     hint: z.string(),
-  }),
+  })
 ])
 
 export type ServerEvent = z.infer<typeof ServerEventSchema>
